47ad4302ab7283b7c1e9c556534f7e474c3cb06a
Pratikesh <pratikeshb9@gmail.com>
2024-12-09 23:24:10 +0530

    Added email support

---
+++ src/main/java/com/pratikesh/project/uber/UberApp/services/implementations/EmailSenderServiceImpl.java
@@ -0,0 +1,51 @@
+package com.pratikesh.project.uber.UberApp.services.implementations;
+
+import com.pratikesh.project.uber.UberApp.services.EmailSenderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailSenderServiceImpl implements EmailSenderService {
+
+    private final JavaMailSender javaMailSender;
+    @Override
+    public void sendEmail(String toEmail, String subject, String body) {
+
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+            simpleMailMessage.setTo(toEmail);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(body);
+
+            javaMailSender.send(simpleMailMessage);
+            log.info("Email sent");
+        }catch (Exception e){
+            log.info("Cannot send mail, "+e.getMessage());
+        }
+    }
+
+    @Override
+    public void sendEmail(String[] toEmail, String subject, String body) {
+
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+            simpleMailMessage.setTo(toEmail);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(body);
+
+            javaMailSender.send(simpleMailMessage);
+            log.info("Email sent");
+        }catch (Exception e){
+            log.info("Cannot send mail, "+e.getMessage());
+        }
+
+    }
+}
--- src/test/java/com/pratikesh/project/uber/UberApp/UberAppApplicationTests.java
+++ src/test/java/com/pratikesh/project/uber/UberApp/UberAppApplicationTests.java
@@ -1,13 +1,19 @@
 package com.pratikesh.project.uber.UberApp;
 
+import com.pratikesh.project.uber.UberApp.services.implementations.EmailSenderServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
 
+	@Autowired
+	private EmailSenderServiceImpl emailSenderService;
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail("ridoye2950@datingel.com",
+				"Testing", "This is a mail for testing purpose");
 	}
 
 }
